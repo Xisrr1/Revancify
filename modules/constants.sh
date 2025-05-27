1f9a528deb23e6f401ef657da5c45fb055657b4c
Xisrr1 <133952156+Xisrr1@users.noreply.github.com>
2025-05-27 19:20:51 +0300

    Update constants.sh

--- modules/constants.sh
+++ modules/constants.sh
@@ -9,10 +9,10 @@ DPI=$(getprop ro.sf.lcd_density)
 
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 
-title='Revancify 𝕏isr'
+title='Revancify Xisr'
 ver="${VERSION:-unknown}"
 cols=$(tput cols)
-pad=$(( cols - ${#title} - ${#ver - 3 } )); [ $pad -lt 0 ] && pad=0
+pad=$(( cols - ${#title} - ${#ver} )); [ $pad -lt 0 ] && pad=0
 backtitle="$title$(printf '%*s' "$pad")$ver"
 
 DIALOG=(dialog --backtitle "$backtitle" --no-shadow --begin 2 0)
