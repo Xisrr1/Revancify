df0863349a57ddd1edc8c0c6f75d5407a4bfc7cc
Xisrr1 <133952156+Xisrr1@users.noreply.github.com>
2025-06-18 17:24:19 +0300

    feat: add beta update toggle

--- modules/preferences.sh
+++ modules/preferences.sh
@@ -2,7 +2,8 @@
 
 configure() {
     local CONFIG_OPTS UPDATED_CONFIG THEME
-    CONFIG_OPTS=("LIGHT_THEME" "$LIGHT_THEME" "PREFER_SPLIT_APK" "$PREFER_SPLIT_APK" "USE_PRE_RELEASE" "$USE_PRE_RELEASE" "LAUNCH_APP_AFTER_MOUNT" "$LAUNCH_APP_AFTER_MOUNT" ALLOW_APP_VERSION_DOWNGRADE "$ALLOW_APP_VERSION_DOWNGRADE")
+    local PREV_BETA="$REVANCIFY_XISR_BETA"
+    CONFIG_OPTS=("LIGHT_THEME" "$LIGHT_THEME" "PREFER_SPLIT_APK" "$PREFER_SPLIT_APK" "USE_PRE_RELEASE" "$USE_PRE_RELEASE" "LAUNCH_APP_AFTER_MOUNT" "$LAUNCH_APP_AFTER_MOUNT" "ALLOW_APP_VERSION_DOWNGRADE" "$ALLOW_APP_VERSION_DOWNGRADE" REVANCIFY_XISR_BETA "$REVANCIFY_XISR_BETA")
 
     readarray -t UPDATED_CONFIG < <(
         "${DIALOG[@]}" \
@@ -23,6 +24,15 @@ configure() {
     done
 
     source .config
+
+    if [ "$PREV_BETA" = "off" ] && [ "$REVANCIFY_XISR_BETA" = "on" ]; then
+        if ! "${DIALOG[@]}" --title "Enable Beta Updates?" --yes-label "Confirm" --no-label "Cancel" --yesno "Are you sure you want to enable beta updates? This will restart Revancify." 0 0; then
+            sed -i "s|^REVANCIFY_XISR_BETA=.*|REVANCIFY_XISR_BETA='off'|" .config
+            source .config
+            return
+        fi
+        exec "$0"
+    fi
 
     [ "$LIGHT_THEME" == "on" ] && THEME="LIGHT" || THEME="DARK"
     export DIALOGRC="config/.DIALOGRC_$THEME"
